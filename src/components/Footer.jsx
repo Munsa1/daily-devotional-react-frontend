228b5639db5ffb57e10b6af68977ff66783b09d3
Munsa1i <munsamibenge65@gmail.com>
2025-09-25 12:05:32 +0200

    Erase previous buggy initialization with new bug free

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="app-color-bg-1">
-      <p className="app-color-text-2 left-margin">
-        © {new Date().getFullYear()} Daily Devotional. All rights reserved.
-      </p>
-    </footer>
-  );
-}
