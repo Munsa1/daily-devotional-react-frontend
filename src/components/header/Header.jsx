5fb8845f0e711cad85629b8a2938900b4f77e85f
Munsa1 <munsamibenge65@gmail.com>
2025-09-25 19:14:33 +0200

    Fix header to display as HTML design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import styles from './header.module.css';
 
-function Header(){
+function Header() {
   return (
-    <header id='header'>
+    <header id="header">
       <div className={styles['mobile-nav-container']}>
-        <div className="mobile-view d-flex app-margin">
+        <div className={`${styles['mobile-view']} d-flex app-margin`}>
           <a href="#">
             <FontAwesomeIcon icon={faBars} />
           </a>
