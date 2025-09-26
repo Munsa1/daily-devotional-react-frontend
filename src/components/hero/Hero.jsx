554892d9dc0323280f07251e0ef53795b44c326f
Munsa1 <munsamibenge65@gmail.com>
2025-09-26 12:20:36 +0200

    Fix Hero section mobile design

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,19 +1,20 @@
 import mountains from "../../assets/images/Mountains.avif";
+import styles from './hero.module.css'
 
 const Hero = () => {
   return (
-    <section className="hero">
-      <div className="hero-image-container">
-        <div id="hero-image">
+    <section className={styles['hero']}>
+      <div className={styles['hero-image-container']}>
+        <div id="hero-image" className={styles['hero-image']}>
           <img src={mountains} alt="Snow Covered Mountains" />
         </div>
       </div>
 
-      <span className="image-text left-margin">
+      <span className={`${styles['image-text']} left-margin`}>
         Snow Covered Mountains
       </span>
 
-      <div className="hero-audio">
+      <div className={styles["hero-audio"]}>
         <audio src="" controls></audio>
       </div>
     </section>
