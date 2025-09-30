153e3726d9378f96258cfa35f112351431e9c1cd
Munsa1 <munsamibenge65@gmail.com>
2025-09-30 14:36:52 +0200

    Add contact section to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ContactUs from './components/ContactUs/ContactUs'
 import Devotional from './components/Devotional/Devotional'
 import Header from './components/header/Header'
 import Hero from './components/hero/Hero'
@@ -9,6 +10,7 @@ function App() {
     <Hero />
     <Devotional />
     <VerseOfTheDay />
+    <ContactUs />
     </>
   )
 }
