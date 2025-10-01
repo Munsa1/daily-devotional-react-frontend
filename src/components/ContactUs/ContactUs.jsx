e4651560e9d96709097eb4c3e7a86e12f963901d
Munsa1 <munsamibenge65@gmail.com>
2025-10-01 20:45:58 +0200

    Fix contact page component

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -59,7 +59,7 @@ export default function ContactUs() {
   };
 
   return (
-    <section className="contact-us d-flex" id="contact">
+    <section classNamen="contact-us d-flex" id="contact">
       <h2 className="left-margin right-margin white">Contact Us</h2>
       <p className="left-margin right-margin white app-color-text-2">
         Feel free to get in touch with us! Whether you'd like to sign up for
