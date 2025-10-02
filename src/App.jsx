95847757d06b989ffbee4f6f1e5fce93d1958e50
Munsa1i <munsamibenge65@gmail.com>
2025-10-02 08:56:29 +0200

    Add other devotions form HTML design with css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import ContactUs from './components/ContactUs/ContactUs'
 import Devotional from './components/Devotional/Devotional'
 import Header from './components/header/Header'
 import Hero from './components/hero/Hero'
+import OtherDevotions from './components/other devotions/OtherDevotions'
 import VerseOfTheDay from './components/VerseOfDay/VerseOfTheDay'
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Devotional />
     <VerseOfTheDay />
     <ContactUs />
+    <OtherDevotions />
     </>
   )
 }
