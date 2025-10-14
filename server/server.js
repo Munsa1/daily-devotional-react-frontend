89f9b2b01456568868f51ea0dcd7d5d1fbf76d67
Munsa1i <munsamibenge65@gmail.com>
2025-10-14 16:17:15 +0200

    Remove commented code from server.js

--- server/server.js
+++ server/server.js
@@ -1,20 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const devotionRoutes = require("./routes/devotionRoutes.js");
-
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/devotion", devotionRoutes);
-
-// const PORT = 5000;
-// app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-
-
-
-
 // server.js
 const express = require("express");
 const fs = require("fs");
